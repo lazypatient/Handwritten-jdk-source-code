2b9452cff3714571839f6f2226b650764c941e82
caojiehui <2239393075@qq.com>
2023-08-15 01:35:11 +0800

    3 ways to get unsafe

--- src/main/java/com/leaf/threadworld/unsafe/MyUnsafe.java
+++ src/main/java/com/leaf/threadworld/unsafe/MyUnsafe.java
@@ -2,11 +2,18 @@ package com.leaf.threadworld.unsafe;
 
 import sun.misc.Unsafe;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
 public class MyUnsafe {
 
-    public static Unsafe getUnsafe() {
+
+    /**
+     * 获取unsafe的三种方式
+     */
+
+    public static Unsafe getUnsafeByReflectProperty() {
 
         Unsafe unsafe;
         try {
@@ -18,6 +25,26 @@ public class MyUnsafe {
             throw new RuntimeException(e);
         }
         return unsafe;
+    }
 
+    public static Unsafe getUnsafeByReflectConstruct() {
+        Unsafe unsafe;
+        try {
+            Constructor<?> constructor = Unsafe.class.getDeclaredConstructors()[0];
+            constructor.setAccessible(true);
+
+            unsafe = (Unsafe) constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+        return unsafe;
+    }
+
+    //验证成功
+    //sun.misc.Unsafe@75b84c92
+    //sun.misc.Unsafe@232204a1
+    public static void main(String[] args) {
+        System.out.println(getUnsafeByReflectConstruct());
+        System.out.println(getUnsafeByReflectProperty());
     }
 }
