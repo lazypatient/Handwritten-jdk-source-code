e02182e036a3162fb64606165eb8dc1aafd6cd08
caojiehui <2239393075@qq.com>
2023-08-17 04:08:39 +0800

    AQS cancelAcquire complex!

--- src/main/java/com/leaf/threadworld/aqs/MyAbstractQueuedSynchronizer.java
+++ src/main/java/com/leaf/threadworld/aqs/MyAbstractQueuedSynchronizer.java
@@ -5,6 +5,8 @@ import com.leaf.threadworld.unsafe.MyUnsafe;
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
 
 /*
     根据可重入锁绘制AQS
@@ -26,13 +28,18 @@ public abstract class MyAbstractQueuedSynchronizer {
 
     private static long headOffset;
 
+    private static long waitStateOffset;
+
+    private static long nextStateOffset;
+
 
     static {
         try {
             stateOffset = UNSAFE.objectFieldOffset(MyAbstractQueuedSynchronizer.class.getDeclaredField("state"));
             tailOffset = UNSAFE.objectFieldOffset(MyAbstractQueuedSynchronizer.class.getDeclaredField("tail"));
             headOffset = UNSAFE.objectFieldOffset(MyAbstractQueuedSynchronizer.class.getDeclaredField("head"));
-
+            waitStateOffset = UNSAFE.objectFieldOffset(Node.class.getDeclaredField("waitStatus"));
+            nextStateOffset = UNSAFE.objectFieldOffset(Node.class.getDeclaredField("next"));
         } catch (NoSuchFieldException e) {
             throw new RuntimeException(e);
         }
@@ -59,6 +66,12 @@ public abstract class MyAbstractQueuedSynchronizer {
         private volatile Node next;
         private volatile Thread thread;
 
+        //节点等待状态
+        private volatile int waitStatus;
+        private static int CANCELED = 1;
+        private static int SIGNAL = -1;//前区节点是SIGNAL 后继节点才可以阻塞
+
+
         public Node() {
 
         }
@@ -68,18 +81,20 @@ public abstract class MyAbstractQueuedSynchronizer {
             this.nextWaiter = mode;
         }
 
-    }
-
-
-    //CAS state
-    public boolean compareAndSetState(int expect, int update) {
-        return UNSAFE.compareAndSwapInt(this, stateOffset, expect, update);
+        public Node preNode() {
+            Node p = prev;
+            if (p == null) {
+                throw new NullPointerException();
+            } else {
+                return p;
+            }
+        }
     }
 
     //抢锁
     public void acquire(int args) {
         //尝试快速获取锁 如果失败需要  1.入队   2.自旋
-        if (!tryAcquire(args) && addNode(Node.EXCLUSIVE) && acquireQuene()) {
+        if (!tryAcquire(args) && acquireQuene(addNode(Node.EXCLUSIVE), args)) {
 
 
         }
@@ -90,7 +105,6 @@ public abstract class MyAbstractQueuedSynchronizer {
      * 入队
      */
     public Node addNode(Node mode) {
-
         //分两种情况 但都是尾部入队
         //1.第一次入队 需要初始化head tail mode节点
         //2.非第一次入队 调整head tail pred next cas 操作
@@ -111,7 +125,7 @@ public abstract class MyAbstractQueuedSynchronizer {
         //走到这里说明  1.队列没有初始化 第一次入队 2.node CAS失败 需要自旋入队
         casEnqueue(node);
 
-        return null;
+        return node;
     }
 
     private void casEnqueue(Node node) {
@@ -134,21 +148,140 @@ public abstract class MyAbstractQueuedSynchronizer {
         }
     }
 
+
+    /**
+     * 自旋 抢锁 阻塞
+     */
+    public boolean acquireQuene(Node node, int args) {
+        boolean failed = true;
+        try {
+            boolean interrupted = false;
+            for (; ; ) {
+                //1.尝试快速获取锁
+                //如果前区节点是头节点 则快速尝试获取锁CAS
+                Node preNode = node.preNode();
+                if (preNode == head && tryAcquire(args)) {
+                    //获取锁成功后
+                    setHead(node);
+                    preNode.next = null;
+                    failed = false;
+                    return interrupted;
+                }
+                //2.阻塞 LockSupport.park
+                if (shouldParkAfterFailedAcquire(preNode, node) && parkAndCheckInterupt()) {
+                    interrupted = true;
+                }
+            }
+        } finally {
+            //取消 就是把自己从对列中摘除
+            if (failed) {
+                cancelAcquire(node);
+            }
+        }
+
+
+    }
+
+    private void cancelAcquire(Node node) {
+        if (node == null) {
+            return;
+        }
+        node.thread = null;
+        Node pred = node.prev;
+        for (; ; ) {
+            if (pred.waitStatus > 0) {
+                //跳过CANCELLED状态的节点
+                node.prev = pred = pred.prev;
+            }
+            Node predNext = pred.next;
+            //设置当前节点状态是CANCELLED
+            node.waitStatus = Node.CANCELED;
+            //分2种情况
+            //1.如果是尾节点 CAS修改
+            if (node == tail && compareAndSetTail(node, pred)) {
+                //把尾节点设置为null
+                compareAndSetNext(pred, predNext, null);
+            }
+            //2.假如改节点是中间节点
+            else {
+                int ws = 0;
+                if (pred != head
+                        && (
+                        (ws = pred.waitStatus) == Node.SIGNAL)
+                        ||
+                        (ws <= 0 && compareAndSetWaitState(pred, ws, Node.SIGNAL))
+                ) {
+
+                }
+            }
+
+
+        }
+
+    }
+
+
+    private boolean parkAndCheckInterupt() {
+        //阻塞的线程会卡在这里等待唤醒
+        //unpark ，interrupt 唤醒方式
+        LockSupport.park(this);
+        return Thread.interrupted();//清除标记位 在某处还原 业务流出不能破坏
+        //这里为什么要清除标记位置呢？
+        //LockSupport 一旦被中断 不清除中断标记 那么将无法在park
+        //C++ openjdk源码 一旦检测 中断标记为 直接结束 不会执行阻塞
+
+    }
+
+    private boolean shouldParkAfterFailedAcquire(Node pred, Node node) {
+
+        int waitStatus = pred.waitStatus;
+        //前驱节点的状态是SIGNAL就阻塞
+        if (waitStatus == Node.SIGNAL) {
+            return true;
+        }
+        if (waitStatus > 0) {//取消状态
+            for (; ; ) {
+                //从右往左一次找 符合SIGNAL状态的等待节点 CANCELED节点直接跳过
+                node.prev = pred = pred.prev;
+                if (pred.waitStatus > 0) {
+                    break;
+                }
+            }
+            pred.next = node;
+            return true;
+        } else {//判断初始状态为0
+            compareAndSetWaitState(pred, waitStatus, Node.SIGNAL);
+        }
+        return false;
+    }
+
+    private boolean compareAndSetWaitState(Node node, int expect, int update) {
+        return UNSAFE.compareAndSwapInt(node, waitStateOffset, expect, update);
+    }
+
+    private boolean compareAndSetNext(Node pred, Node expect, Object update) {
+        return UNSAFE.compareAndSwapObject(pred, nextStateOffset, expect, update);
+    }
+
     private boolean compareAndSetHead(Node update) {
         return UNSAFE.compareAndSwapObject(this, headOffset, null, update);
     }
 
     private boolean compareAndSetTail(Node expect, Node update) {
-
         return UNSAFE.compareAndSwapObject(this, tailOffset, expect, update);
     }
 
-    /**
-     * 自旋
-     */
-    public boolean acquireQuene() {
+    //CAS state
+    public boolean compareAndSetState(int expect, int update) {
+        return UNSAFE.compareAndSwapInt(this, stateOffset, expect, update);
+    }
 
-        return false;
+    private void setHead(Node node) {
+        //head 指向成功获取锁的node
+        head = node;
+        //头节点线程永远锁null
+        node.thread = null;
+        node.prev = null;
     }
 
 
@@ -184,13 +317,6 @@ public abstract class MyAbstractQueuedSynchronizer {
         this.state = state;
     }
 
-    public Node getHead() {
-        return head;
-    }
-
-    public void setHead(Node head) {
-        this.head = head;
-    }
 
     public Node getTail() {
         return tail;
--- src/main/java/com/leaf/threadworld/test/MyThreadPoolExecutorTest.java
+++ src/main/java/com/leaf/threadworld/test/MyThreadPoolExecutorTest.java
@@ -1,7 +1,28 @@
 package com.leaf.threadworld.test;
 
+import java.util.concurrent.locks.LockSupport;
+
 public class MyThreadPoolExecutorTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Thread t2 = new Thread(() -> {
+            LockSupport.park();
+            System.out.println("1");
+            LockSupport.park();
+            System.out.println("2");
+
+
+        });
+        t2.start();
+        System.out.println(888);
+
+        Thread.sleep(200);
+
+        LockSupport.unpark(t2);
+        Thread.sleep(5000);
+        LockSupport.unpark(t2);
+
 
     }
 }
