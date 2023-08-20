a902ec3ef6578d634b5a358f739368ce3f0ddd8f
houlixie <2239393075@qq.com>
2023-08-20 19:34:01 +0800

    SemaphoreDemo add interrupt !

--- src/main/java/com/leaf/jdk/demo/SemaphoreDemo.java
+++ src/main/java/com/leaf/jdk/demo/SemaphoreDemo.java
@@ -12,38 +12,47 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class SemaphoreDemo {
     private static boolean isAcquired = false;
+    private static final Semaphore semaphore = new Semaphore(1);
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //分配2个资源
-        Semaphore semaphore = new Semaphore(1);
+        Thread t1 = new Thread(SemaphoreDemo::task, "001");
+        Thread t2 = new Thread(SemaphoreDemo::task, "002");
 
-        for (int i = 0; i < 2; i++) {
-            new Thread(() -> {
-                try {
-                    //消耗一个资源 state-1
-                    semaphore.acquire();
-                    isAcquired = true;
-                    //业务
-                    System.out.println(Thread.currentThread().getName() + "is running!");
-                    TimeUnit.SECONDS.sleep(5);
+        t1.start();
+        TimeUnit.MILLISECONDS.sleep(100);
+        t2.start();
+        t2.interrupt();
 
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                } finally {
-                    //释放资源
-                    //state+1
-                    if (!isAcquired) {
-                        System.out.println("当前线程" + Thread.currentThread().getName() + "发生了中断！");
-                    }
-                    //正常释放资源
-                    semaphore.release();
-                }
 
-            }, "tName===" + i).start();
+    }
+
+    public static void task() {
+        try {
+            //消耗一个资源 state-1
+            semaphore.acquire();
+            //业务
+            System.out.println(Thread.currentThread().getName() + "  is running!");
+            TimeUnit.SECONDS.sleep(5);
+            isAcquired = true;
+
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //释放资源
+            //state+1
+            if (!isAcquired) {
+                System.out.println("当前线程" + Thread.currentThread().getName() + "发生了中断！资源还剩余" + semaphore.availablePermits());
+
+            } else {
+                //正常释放资源
+                System.out.println(Thread.currentThread().getName() + "正常释放了锁！");
+                semaphore.release();
+            }
+
         }
-
-
     }
 }
